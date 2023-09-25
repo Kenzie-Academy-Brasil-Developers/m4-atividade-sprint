b4b96eb9a5d5d0aa56738e8756340bddb971bea1
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-09-25 15:48:58 -0300

    atividade4

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Olá Kenzie, me chamo Alex!")
+class Student{
+    name: string;
+    module: string;
+    grade: number | null = null;
+
+    constructor(name: string, module: string) {
+        this.name = name;
+        this.module = module;        
+    }
+
+    sayHello(){
+        console.log(`Olá me chamo ${this.name} e estou no módulo ${this.module}.`);
+    }
+
+    setGrade(grade: number){
+        this.grade = grade;
+    }
+    
+    getGrade(){
+        console.log(`Nota: ${this.grade}`);
+    }
+}
+
+const student = new Student("Osvaldo", "M4");
+student.sayHello();
+student.setGrade(10);
+student.getGrade();
